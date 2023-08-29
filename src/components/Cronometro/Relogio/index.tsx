b7a49aa97168c8c5abf7a77e1e3dc74b4ee1bc37
Miguel Cp <miguelcp716@gmail.com>
2023-08-29 11:16:21 -0300

    continuação pre-correção

---
+++ src/components/Cronometro/Relogio/index.tsx
@@ -0,0 +1,14 @@
+import Style from "./Relogio.module.scss";
+
+export default function Relogio(){
+    return(
+        <>
+        <span className={Style.relogioNumero}>0</span>
+        <span className={Style.relogioNumero}>0</span>
+        <span className={Style.relogioDivisao}>:</span>
+        <span className={Style.relogioNumero}>0</span>
+        <span className={Style.relogioNumero}>0</span>
+
+        </>
+    )
+}
